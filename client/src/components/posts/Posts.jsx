01c2c5a1486ad83d8277b201a06ed5b9b5db6e83
Mabishashini N <118960931+Mabishashini@users.noreply.github.com>
2024-03-11 10:31:17 +0530

    checking 2

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -5,6 +5,7 @@ import { Post } from '../post/Post'
 export const Posts = ({posts}) => {
   return (
     <div className='posts'>
+      {console.log(posts)}
       {posts.map((p) => (
         
         <Post key ={p._id} post = {p}/>
